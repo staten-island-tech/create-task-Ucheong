90a806179e4c0c0f8d25140252c0955c4917cb03
Ucheong <ucheongsou@gmail.com>
2023-02-28 10:13:34 -0500

    create task

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -2,6 +2,8 @@ const DOMSelectors = {
   grid: document.querySelector(".game"),
   start: document.querySelector(".start"),
   timer: document.querySelector(".timer"),
+  timerbox: document.querySelector(".timerbox"),
+  display: document.querySelector(".display"),
 };
 
 export { DOMSelectors };
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,14 +2,20 @@ import "./dom";
 import { DOMSelectors } from "./dom";
 import "../styles/style.css";
 import { array } from "./array";
-// import "./array";
-// import { array } from "./array";
+import "./array";
 
 document.querySelector(".start").addEventListener("click", function () {
+  DOMSelectors.start.remove();
+  DOMSelectors.explaination.remove();
+  DOMSelectors.timerbox.insertAdjacentHTML(
+    "afterbegin",
+    `<p class="timer">1:00</p>`
+  );
   // DOMSelectors.body = ``;
   // board();
   setInterval(timer, 1000);
   timer();
+  //remove the start button
 });
 
 // function board() {}
@@ -25,15 +31,16 @@ function timer() {
     seconds = time % 60;
   }
 
-  DOMSelectors.timer.innerHTML = `${minutes}:${seconds}`;
+  DOMSelectors.timerbox.innerHTML = `${minutes}:${seconds}`;
   time--;
 }
 
 array.sort(() => 0.5 - Math.random());
 
-// function grid() {
-//   .display.insertAdjacentHTML;
-// }
+function grid() {}
+grid();
 // function flipcard() {}
 
-function check() {}
+// function check() {}
+
+// <button class="home"><i class="fa fa-home"></i></button>
