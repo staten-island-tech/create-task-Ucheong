962649a6c9ab74606421ec57c1a236c1a7690706
Ucheong <ucheongsou@gmail.com>
2023-02-27 23:23:53 -0500

    create task

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,27 +1,39 @@
 import "./dom";
 import { DOMSelectors } from "./dom";
 import "../styles/style.css";
+import { array } from "./array";
 // import "./array";
 // import { array } from "./array";
 
 document.querySelector(".start").addEventListener("click", function () {
   // DOMSelectors.body = ``;
-  // memorygame();
+  // board();
   setInterval(timer, 1000);
   timer();
 });
 
-// function memorygame() {}
+// function board() {}
 
 let time = 60;
 
 function timer() {
   let minutes = Math.floor(time / 60);
   let seconds = time % 60;
-  seconds = seconds < 10 ? `0` + seconds : seconds;
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  } else {
+    seconds = time % 60;
+  }
 
   DOMSelectors.timer.innerHTML = `${minutes}:${seconds}`;
   time--;
 }
 
+array.sort(() => 0.5 - Math.random());
+
+// function grid() {
+//   .display.insertAdjacentHTML;
+// }
 // function flipcard() {}
+
+function check() {}
